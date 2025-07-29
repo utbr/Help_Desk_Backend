f76f1646ae3e7100451d63b027f4329e692e3fad
Leonardo Felipe Hopata Machado <140458056+utbr@users.noreply.github.com>
2025-07-28 22:06:59 -0300

    Atualização de CORS
    Para desenvolvimento local, permiti qualquer origem, ajustar em produção

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app.include_router(router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173/"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
